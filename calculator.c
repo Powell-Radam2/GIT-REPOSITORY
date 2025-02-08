efe968627bcf9b8d767d976edb92687b1a8c61d1
Avilaski <gregavilajr@gmail.com>
2025-02-08 16:02:35 +0800

    Add files via upload

--- calculator.c
+++ calculator.c
@@ -1,44 +1,51 @@
 #include <stdio.h>
 float calculate (float result, float num2, char operation)
 {
-    float result;
+    float result2 = 0;
     
-    if (operation == '+');
-        result = num1 + num2;
-    if (operation == '-');
-        result = num1 - num2;
-    if (operation == '*');
-        result = num1 * num2;
-    if (operation == '/');
-        result = num1 / num2;
+    if (operation == '+')
+        result2 = num2 + result;
+    if (operation == '-')
+        result2 = result - num2;
+    if (operation == '*')
+        result2 = result * num2;
+    if (operation == '/')
+        result2 = result / num2;
 
-    return result;
+    return result2;
 }
 
 int main ()
 {
 
 float result = 0;
-int i;
 char operation = 'a';
 float num2;
 
-while (operation != '=');
+for (int i = 0; operation != '='; i++)
 {
-    scanf("%d", &result);
-    scanf("%c", &operation);
-    scanf("%d", &num2);
-    result = calculate(result, num2, operation);
-}
-
-printf("%d", result);
+    if (i == 0)
+    {
+        scanf("%f", &result);
+    }
         
+    if (i > 0)
+    {
+        getchar();
+        scanf("%c", &operation);
+       
+        if(operation == '=')
+            break;
+        
+        getchar();
+        scanf("%f", &num2);
+
+        result = calculate(result, num2, operation);
+        //printf("= %f\n", result);
+    }
     
-    
-
-
-
-
-
-
 }
+
+printf("%f", result);
+}
+
